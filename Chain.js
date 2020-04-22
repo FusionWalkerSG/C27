9d6df53e195d50bbf1fd36ee555487746d88cb82
FusionWalkerSG <golwara.shaurya@gmail.com>
2020-04-22 14:45:30 +0530

    kung foo bird

---
+++ Chain.js
@@ -0,0 +1,21 @@
+class Chain{
+    constructor(bodyA,bodyB){
+       var options = {
+           bodyA:bodyA,
+           bodyB:bodyB,
+           stiffness : 0.3 ,
+           length : 9 ,   
+       }
+       this.chain = Constraint.create(options);
+       World.add(world,this.chain);
+    }
+    display(){
+       push();
+       var pointA = this.chain.bodyA.position;
+       var pointB = this.chain.bodyB.position;
+       fill("red");
+       strokeWeight(10);
+       line (pointA.x,pointA.y,pointB.x,pointB.y)
+       pop();
+    }
+}
